df2ad4af3c50b4edad7614415b74241c7dc41857
prabhu5013 <prabhu5013@gmail.com>
2021-07-02 23:33:23 +0530

    Added store dev tools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,18 @@ import { collectionReducer } from './state/collection.reducer';
 import { BookListComponent } from './book-list/book-list.component';
 import { BookCollectionComponent } from './book-collection/book-collection.component';
 import { HttpClientModule } from '@angular/common/http';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    StoreModule.forRoot({ books: booksReducer, collection: collectionReducer })
+    StoreModule.forRoot({ books: booksReducer, collection: collectionReducer }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 20,
+      logOnly: false
+    })
   ],
   declarations: [AppComponent, BookListComponent, BookCollectionComponent],
   bootstrap: [AppComponent]
